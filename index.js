f4736745f8138a1dec6c9aa8a836e66626a4c746
anushkxa <anushka082006@gmail.com>
2025-08-11 21:30:38 +0530

    Final Commit

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app= express();
 const mongoose = require("mongoose");
 const path=require("path");
 const Chat=require("./models/chat.js");
+const methodOverride= require("method-override");
 
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 app.get("/chats/new",(req,res)=>{
     res.render("new.ejs");
@@ -51,6 +53,27 @@ app.get("/",(req,res)=>{
     res.send("app is working");
 })
 
+app.get("/chats/:id",async(req,res)=>{
+    let {id}= req.params;
+    let edd= await Chat.findById(id);
+    res.render("edit.ejs",{edd});
+})
+
+app.put("/chats/:id",async (req,res)=>{
+    let{id}=req.params;
+    let {msg:newMsg}= req.body;
+    console.log(newMsg);
+    let updateChat= await Chat.findByIdAndUpdate(id,{msg:newMsg},{runValidators:true,new:true});
+    console.log("Updated Chat");
+    res.redirect("/chats");
+})
+
+
+app.delete("/chats/:id",async(req,res)=>{
+    let {id}=req.params;
+    let deletedChat=await Chat.findByIdAndDelete(id);
+    res.redirect("/chats");
+})
 app.listen(3000,()=>{
     console.log("port is listening");
 })
