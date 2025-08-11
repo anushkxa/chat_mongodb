3aa79557fd12e784243559fb139497bd38b531c8
anushkxa <anushka082006@gmail.com>
2025-08-11 14:47:26 +0530

    Add new chat

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Chat=require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 app.get("/chats/new",(req,res)=>{
     res.render("new.ejs");
@@ -25,6 +26,25 @@ main().then(()=>{
 app.get("/chats",async(req,res)=>{
     let chats= await Chat.find();
     res.render("index.ejs",{chats});
+})
+
+
+app.post("/chats",(req,res)=>{
+    let{from,to,msg}=req.body;
+    let newChat= new Chat({
+        from:from,
+        to:to,
+        msg:msg,
+        created_at:new Date()
+    })
+    newChat.save().
+    then((res)=>{
+        console.log("Chat was Saved")
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+    res.redirect("/chats");
 })
 
 app.get("/",(req,res)=>{
